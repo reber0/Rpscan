ca9ac2e7416dd8afaf03afef18dc26f07aff426b
reber <reber0ask@protonmail.com>
2020-01-21 15:12:39 +0800

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 @Author: reber
 @Mail: reber0ask@qq.com
 @Date: 2019-12-28 17:01:44
-@LastEditTime: 2020-01-18 02:39:08
+@LastEditTime : 2020-01-21 15:12:20
 '''
 import sys
 sys.dont_write_bytecode = True  # 不生成pyc文件
@@ -32,7 +32,7 @@ usage:
     >>> from Rpscan import NmapGetPortService
     >>> ngps = NmapGetPortService(ip_port_dict={'59.108.35.243': [80, 22]}, thread_num=10)
     >>> port_service_list = ngps.run()
-    >>> pprint.pprint(a)
+    >>> pprint.pprint(port_service_list)
     {'59.108.35.243': [{'name': 'ssh',
                         'port': 22,
                         'product': 'OpenSSH',
