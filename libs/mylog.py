43ce49856ea9774671958e82d2017ccfa5b4aafa
reber <reber0ask@protonmail.com>
2020-06-17 00:55:59 +0800

    update

--- libs/mylog.py
+++ libs/mylog.py
@@ -4,7 +4,7 @@
 @Author: reber
 @Mail: reber0ask@qq.com
 @Date: 2019-07-16 22:31:00
-@LastEditTime : 2020-06-11 10:27:12
+@LastEditTime : 2020-06-16 13:17:05
 '''
 
 import logging
@@ -26,13 +26,13 @@ class MyLog(object):
 
         # 创建 stream hander
         sh = self.__stream_hander(loglevel)
-        if not self.logger.handlers: # 如果 handler 没有时才添加，为了解决日志重复问题
+        if not self.logger.handlers:  # 如果 handler 没有时才添加，为了解决日志重复问题
             self.logger.addHandler(sh)
 
         # 创建 file hander
         if logfile:
             fh = self.__file_hander(logfile)
-            if len(self.logger.handlers) == 1: # 解决日志重复问题
+            if len(self.logger.handlers) == 1:  # 解决日志重复问题
                 self.logger.addHandler(fh)
 
     def __file_hander(self, logfile):
