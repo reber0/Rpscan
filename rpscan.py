d3c3ed47a7fc8a88ecc284aa7ac0c222f6aeed77
reber <reber0ask@protonmail.com>
2020-06-17 00:54:20 +0800

    update rpscan.py

--- rpscan.py
+++ rpscan.py
@@ -4,54 +4,59 @@
 @Author: reber
 @Mail: reber0ask@qq.com
 @Date: 2019-05-23 09:52:13
-@LastEditTime : 2020-01-23 13:52:38
+@LastEditTime : 2020-06-17 00:20:01
 '''
 import sys
 sys.dont_write_bytecode = True  # 不生成pyc文件
+
 from libs.util import get_content
-from libs.parse import Parser
-from libs.parse import ParseTarget
-from libs.check_host_live import CheckHostLive
-from libs.port_scan import PortScan
-from libs.get_service import NmapGetPortService
+from libs.initialize import init_config
+from libs.initialize import init_cmd_args
+from libs.data import config
+
+from modules.check_live import CheckHostLive
+from modules.get_service import NmapGetPortService
+from modules.masscan_s import MasscanScan
+from modules.async_s import AsyncTcpScan
+from modules.nmap_s import NmapScan
 
 
 def main():
-    args = Parser().init()
-    # print(args)
+    # 引入配置文件中的配置
+    init_config("config.py")
 
-    pt = ParseTarget()
-    if args.get("target"):
-        ip_list = pt.parse_target(args.get("target"))
-    elif args.get("target_filename"):
-        target_list = get_content(args.get("target_filename"))
-        ip_list = pt.parse_target(target_list)
+    # 解析命令行参数
+    init_cmd_args()
 
-    if args.get("checklive"):
-        chl = CheckHostLive(ip_list=ip_list)
-        live_host = chl.run()
-        # print("All Host: {}, Live Host: {}".format(
-        #     len(ip_list), len(live_host)))
+    # 端口扫描
+    if config.checklive:
+        chl = CheckHostLive(ip_list=config.ip_list)
+        config.target_host = chl.run()
     else:
-        live_host = ip_list
-
-    if len(live_host) < 1:
+        config.target_host = config.ip_list
+    if len(config.target_host) < 1:
         exit()
+    
+    # print(config.target_host)
+    # exit()
 
-    if args.get("scantype") == "masscan":
-        ps = PortScan(ip_list=live_host, all_ports=args.get(
-            "is_all_ports"), rate=args.get("rate"))
-        port_open_dict = ps.masscan_scan()
-    elif args.get("scantype") == "tcp":
-        ps = PortScan(ip_list=live_host, all_ports=args.get(
-            "is_all_ports"), rate=args.get("rate"))
-        port_open_dict = ps.async_tcp_port_scan()
+    # 端口扫描
+    if config.scantype == "masscan":
+        m_scan = MasscanScan()
+        open_port_dict = m_scan.run()
+    elif config.scantype == "nmap":
+        n_scan = NmapScan()
+        open_port_dict = n_scan.run()
+    elif config.scantype == "tcp":
+        a_scan = AsyncTcpScan()
+        open_port_dict = a_scan.run()
 
-    if args.get("service") and len(port_open_dict) > 0:
-        # 对扫描出来的端口进行服务识别后写入数据库
-        ngps = NmapGetPortService(
-            ip_port_dict=port_open_dict, thread_num=args.get("thread"))
+    # 对扫描出来的端口进行服务识别
+    if config.get_service and len(open_port_dict) > 0:
+        ngps = NmapGetPortService(ip_port_dict=open_port_dict)
         port_service_list = ngps.run()
+
+
 
 
 if __name__ == "__main__":
